1c03fc3145db20f6e04824cf13ab77a284ba99da
osteoporosis12 <pablexsanabria@gmail.com>
2023-09-23 00:28:13 -0400

    cositas

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -2,24 +2,19 @@ import { MapContainer, TileLayer, useMap,Marker,Popup } from 'react-leaflet'
 import {Icon} from 'leaflet'
 import { MapPinIcon } from '@heroicons/react/20/solid'
 import 'leaflet/dist/leaflet.css'
-export default function Map(props){
-    const {latitud, longitud}=props
-    const ubi=[latitud, longitud]
-    const iconPin=new Icon({
-        iconUrl:"pin.png",
-        iconSize:[32,50]
-    })
-    return(
-        <MapContainer center={ubi} zoom={13} scrollWheelZoom={false} className=' w-96 h-96 '>
+export default function Map(){
+
+  return(
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
   <TileLayer
-    attribution='<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url='https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png'
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={ubi} icon={iconPin}>
+  <Marker position={[51.505, -0.09]}>
     <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup>
   </Marker>
 </MapContainer>
-    )
+  )
 }
--- src/pages/Ia.jsx
+++ src/pages/Ia.jsx
@@ -2,7 +2,7 @@ export const Ia = () => {
     return (
         <>
             <h1 className="text-green-500">Ia</h1>
-            <img src="" alt="" />
+            <img src="http://192.168.1.97:81/stream" alt="" />
         </>
     )
 }
